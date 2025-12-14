4f7b28925ad5fab2a6ce8d428656eaf83d638170
ale21-30 <ale512127@gmail.com>
2025-12-14 11:46:59 -0500

    feat: listar productos en el frontend

--- BITES-BACKEND/public/index.php
+++ BITES-BACKEND/public/index.php
@@ -1,9 +1,41 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+*/
 
-$app->run();
+$app = Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function ($middleware) {
+        //
+    })
+    ->withExceptions(function ($exceptions) {
+        //
+    })
+    ->create();
+
+/*
+|--------------------------------------------------------------------------
+| Handle The Request
+|--------------------------------------------------------------------------
+*/
+
+$request = Request::capture();
+$response = $app->handle($request);
+$response->send();
+
+$app->terminate($request, $response);
